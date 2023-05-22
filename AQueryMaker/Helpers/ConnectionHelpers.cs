636bbba4d8265df3b99209438f54ba5032d4ce63
asimgunduz <a.gunduz@live.com>
2023-05-22 10:44:31 +0300

    initial

---
+++ AQueryMaker/Helpers/ConnectionHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Common;
+using System.Data.SqlClient;
+using AQueryMaker.Interfaces;
+using AQueryMaker.MSSql;
+using AQueryMaker.Oracle;
+using Oracle.ManagedDataAccess.Client;
+
+namespace AQueryMaker.Helpers;
+
+public class ConnectionHelpers
+{
+    // public static IDatabaseManager GetConnection(DbContext context)
+    // {
+    //     if (context.Database.ProviderName.Contains("Microsoft", StringComparison.OrdinalIgnoreCase))
+    //         return new SqlServerManager(context.Database.GetDbConnection());
+    //
+    //     if (context.Database.ProviderName.Contains("Oracle", StringComparison.OrdinalIgnoreCase))
+    //         return new OracleServerManager(context.Database.GetDbConnection());
+    //
+    //     throw new NotSupportedException();
+    // }
+
+    // public static IDatabaseManager GetConnection(DbConnection dbConnection)
+    // {
+    //     if (dbConnection is SqlConnection)
+    //         return new SqlServerManager(dbConnection);
+    //
+    //     if (dbConnection is OracleConnection)
+    //         return new OracleServerManager(dbConnection);
+    //
+    //     throw new NotSupportedException();
+    // }
+}
+
+
---
+++ AQueryMaker/SqlQueryFactory.cs
@@ -0,0 +1,65 @@
+﻿using System.Data;
+using System.Data.Common;
+using AQueryMaker.Interfaces;
+
+namespace AQueryMaker;
+
+public class SqlQueryFactory : IDatabaseManager
+{
+    private readonly IDatabaseManager Manager;
+
+    public DbConnection Connection => Manager.Connection;
+
+    public SqlQueryFactory(IDatabaseManager manager)
+    {
+        Manager = manager;
+    }
+
+    public Task<IDictionary<string, object>> InsertAsync(string TableName, IDictionary<string, object> Model, CommandBehavior CommandBehavior = CommandBehavior.Default)
+    {
+        return Manager.InsertAsync(TableName, Model, CommandBehavior);
+    }
+
+    public Task<IDictionary<string, object>> DeleteAsync(string TableName, IDictionary<string, object> Model, CommandBehavior CommandBehavior = CommandBehavior.Default)
+    {
+        return Manager.DeleteAsync(TableName, Model, CommandBehavior);
+    }
+
+    public Task<IDictionary<string, object>> UpdateAsync(string TableName, IDictionary<string, object> Model, CommandBehavior CommandBehavior = CommandBehavior.Default)
+    {
+        return Manager.UpdateAsync(TableName, Model, CommandBehavior);
+    }
+
+    public Task<List<IDictionary<string, object>>> QueryAsync(string Query, params (string Key, object Value)[] WhereStatementParameters)
+    {
+        return Manager.QueryAsync(Query, WhereStatementParameters);
+    }
+
+    public Task<List<IDictionary<string, object>>> QueryAsync(string Query, CommandBehavior CommandBehavior, CommandType CommandType, params (string Key, object Value)[] WhereStatementParameters)
+    {
+        return Manager.QueryAsync(Query, CommandBehavior, CommandType, WhereStatementParameters);
+    }
+    public Task<List<IDictionary<string, object>>> QueryAsync(string Query, params KeyValuePair<string, object>[] WhereStatementParameters)
+    {
+        return Manager.QueryAsync(Query, WhereStatementParameters);
+    }
+
+    public Task<List<IDictionary<string, object>>> QueryAsync(string Query, CommandBehavior CommandBehavior, CommandType CommandType, params KeyValuePair<string, object>[] WhereStatementParameters)
+    {
+        return Manager.QueryAsync(Query, CommandBehavior, CommandType, WhereStatementParameters);
+    }
+
+    public Task<List<IDictionary<string, object>>> GetStoredProcedureFieldMetaDataAsync(string ProcedureName)
+    {
+        return Manager.GetStoredProcedureFieldMetaDataAsync(ProcedureName);
+    }
+
+    public Task<List<IDictionary<string, object>>> GetMethodParameters(string MethodName)
+    {
+        return Manager.GetMethodParameters(MethodName);
+    }
+
+    
+}
+
+
