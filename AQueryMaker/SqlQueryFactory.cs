a4739839b19064cdd2a933e3abc6973135b92d65
asimgunduz <a.gunduz@live.com>
2023-11-25 00:13:48 +0200

    FromVs

--- AQueryMaker/SqlQueryFactory.cs
+++ AQueryMaker/SqlQueryFactory.cs
@@ -15,51 +15,101 @@ public class SqlQueryFactory : IDatabaseManager
         Manager = manager;
     }
 
+    /// <summary>
+    /// Asynchronously inserts a new record into the specified table using the provided model.
+    /// </summary>
     public Task<IDictionary<string, object>> InsertAsync(string TableName, IDictionary<string, object> Model, CommandBehavior CommandBehavior = CommandBehavior.Default)
     {
         return Manager.InsertAsync(TableName, Model, CommandBehavior);
     }
 
+    /// <summary>
+    /// Asynchronously deletes records from the specified table based on the provided model.
+    /// </summary>
     public Task<IDictionary<string, object>> DeleteAsync(string TableName, IDictionary<string, object> Model, CommandBehavior CommandBehavior = CommandBehavior.Default)
     {
         return Manager.DeleteAsync(TableName, Model, CommandBehavior);
     }
 
+    /// <summary>
+    /// Asynchronously updates records in the specified table using the provided model.
+    /// </summary>
     public Task<IDictionary<string, object>> UpdateAsync(string TableName, IDictionary<string, object> Model, CommandBehavior CommandBehavior = CommandBehavior.Default)
     {
         return Manager.UpdateAsync(TableName, Model, CommandBehavior);
     }
 
-    public Task<List<IDictionary<string, object>>> QueryAsync(string Query, params (string Key, object Value)[] WhereStatementParameters)
-    {
-        return Manager.QueryAsync(Query, WhereStatementParameters);
-    }
+   
 
-    public Task<List<IDictionary<string, object>>> QueryAsync(string Query, CommandBehavior CommandBehavior, CommandType CommandType, params (string Key, object Value)[] WhereStatementParameters)
-    {
-        return Manager.QueryAsync(Query, CommandBehavior, CommandType, WhereStatementParameters);
-    }
+    /// <summary>
+    /// Asynchronously executes a query with optional where statement parameters and returns the result set as a list of dictionaries.
+    /// </summary>
     public Task<List<IDictionary<string, object>>> QueryAsync(string Query, params KeyValuePair<string, object>[] WhereStatementParameters)
     {
         return Manager.QueryAsync(Query, WhereStatementParameters);
     }
 
+    /// <summary>
+    /// Asynchronously executes a query with optional where statement parameters and command behavior, command type, and returns the result set as a list of dictionaries.
+    /// </summary>
     public Task<List<IDictionary<string, object>>> QueryAsync(string Query, CommandBehavior CommandBehavior, CommandType CommandType, params KeyValuePair<string, object>[] WhereStatementParameters)
     {
         return Manager.QueryAsync(Query, CommandBehavior, CommandType, WhereStatementParameters);
     }
 
-    public Task<List<IDictionary<string, object>>> GetStoredProcedureFieldMetaDataAsync(string ProcedureName)
+    /// <summary>
+    /// Asynchronously retrieves metadata for a stored procedure's fields.
+    /// Dictionary Keys
+    /// NAME, SYSTEM_TYPE_NAME
+    /// </summary>
+    public Task<List<IDictionary<string, object>>> GetStoredProcedureFieldsAsync(string ProcedureName)
     {
-        return Manager.GetStoredProcedureFieldMetaDataAsync(ProcedureName);
+        return Manager.GetStoredProcedureFieldsAsync(ProcedureName);
     }
 
+    /// <summary>
+    /// Asynchronously retrieves metadata for a method's parameters.
+    /// </summary>
     public Task<List<IDictionary<string, object>>> GetMethodParameters(string MethodName)
     {
         return Manager.GetMethodParameters(MethodName);
     }
 
-    
+    public IAsyncEnumerable<List<IDictionary<string, object>>> StreamAsync(string Query, params KeyValuePair<string, object>[] WhereStatementParameters)
+    {
+        return Manager.StreamAsync(Query, WhereStatementParameters);
+    }
+
+    public IAsyncEnumerable<List<IDictionary<string, object>>> StreamAsync(string Query, int ItemPerPage, params KeyValuePair<string, object>[] WhereStatementParameters)
+    {
+        return Manager.StreamAsync(Query, ItemPerPage, WhereStatementParameters);
+    }
+
+    /// <summary>
+    /// Retrieves list of Stored Procedures
+    /// Dictionary Keys : SP_NAME
+    /// </summary>
+    /// <returns></returns>
+    public Task<List<IDictionary<string, object>>> GetStoredProcedures()
+    {
+        return Manager.GetStoredProcedures();
+    }
+
+    public Task<List<IDictionary<string, object>>> GetTableFieldsAsync(string TableName)
+    {
+        return Manager.GetTableFieldsAsync(TableName);
+    }
+
+    public Task<List<IDictionary<string, object>>> GetTableListAsync()
+    {
+        return Manager.GetTableListAsync();
+
+    }
+
+    public Task<List<IDictionary<string, object>>> GetStoredProcedureParametersAsync(string StoredProcedureName)
+    {
+        return Manager.GetStoredProcedureParametersAsync(StoredProcedureName);
+    }
 }
 
 
