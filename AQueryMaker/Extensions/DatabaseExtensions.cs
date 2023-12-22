aa4d845ca2d7e609e7b91ce36e936bbff5155625
asimgunduz <a.gunduz@live.com>
2023-12-22 15:55:03 +0200

    latest

--- AQueryMaker/Extensions/DatabaseExtensions.cs
+++ AQueryMaker/Extensions/DatabaseExtensions.cs
@@ -1,29 +1,19 @@
 using System.Data.Common;
 using AQueryMaker.MSSql;
 using AQueryMaker.Oracle;
-using Microsoft.EntityFrameworkCore;
 
 namespace AQueryMaker.Extensions;
 
-// ReSharper disable once UnusedType.Global
 public static class DatabaseExtensions
 {
-    // ReSharper disable once UnusedMember.Global
-    public static QueryManager Manager(this DbContext context)
+    
+    public static SqlServerManager SqlManager(this DbConnection connection)
     {
-        return context.Database.ProviderName switch
-        {
-            "Microsoft.EntityFrameworkCore.SqlServer" => new QueryManager(SqlManager(context.Database.GetDbConnection())),
-            "Oracle.EntityFrameworkCore" => new QueryManager(OracleManager(context.Database.GetDbConnection())),
-            _ => default
-        };
+        return new SqlServerManager(connection);
     }
-
-    // ReSharper disable once UnusedMember.Global
-    public static SqlServerManager SqlManager(this DbConnection connection) => new(connection);
     
-    // ReSharper disable once UnusedMember.Global
-    public static OracleServerManager OracleManager(this DbConnection connection) => new(connection);
-    
-  
+    public static OracleServerManager OracleManager(this DbConnection connection)
+    {
+        return new OracleServerManager(connection);
+    }
 }
