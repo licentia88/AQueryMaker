b116bcae9828712ed2204968bf6006834df3b0f4
asimgunduz <a.gunduz@live.com>
2023-12-22 16:05:00 +0200

    minor fixes and feature improvements

--- AQueryMaker/Extensions/DatabaseExtensions.cs
+++ AQueryMaker/Extensions/DatabaseExtensions.cs
@@ -1,11 +1,16 @@
 using System.Data.Common;
 using AQueryMaker.MSSql;
 using AQueryMaker.Oracle;
+using Microsoft.EntityFrameworkCore;
 
 namespace AQueryMaker.Extensions;
 
 public static class DatabaseExtensions
 {
+    public static SqlServerManager SqlManager(this DbContext context)
+    {
+        return new SqlServerManager(context.Database.GetDbConnection());
+    }
     
     public static SqlServerManager SqlManager(this DbConnection connection)
     {
@@ -15,5 +20,9 @@ public static class DatabaseExtensions
     public static OracleServerManager OracleManager(this DbConnection connection)
     {
         return new OracleServerManager(connection);
+    }
+    public static OracleServerManager OracleManager(this DbContext context)
+    {
+        return new OracleServerManager(context.Database.GetDbConnection());
     }
 }
