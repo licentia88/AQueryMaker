2af3e50524864d98f69af97959c1c72c843c07f7
asimgunduz <a.gunduz@live.com>
2023-12-23 12:14:01 +0200

    fix

--- AQueryMaker/Extensions/DatabaseExtensions.cs
+++ AQueryMaker/Extensions/DatabaseExtensions.cs
@@ -5,22 +5,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AQueryMaker.Extensions;
 
+// ReSharper disable once UnusedType.Global
 public static class DatabaseExtensions
 {
+    // ReSharper disable once UnusedMember.Global
     public static SqlServerManager SqlManager(this DbContext context)
     {
         return new SqlServerManager(context.Database.GetDbConnection());
     }
     
+    // ReSharper disable once UnusedMember.Global
     public static SqlServerManager SqlManager(this DbConnection connection)
     {
         return new SqlServerManager(connection);
     }
     
+    // ReSharper disable once UnusedMember.Global
     public static OracleServerManager OracleManager(this DbConnection connection)
     {
         return new OracleServerManager(connection);
     }
+    
+    // ReSharper disable once UnusedMember.Global
     public static OracleServerManager OracleManager(this DbContext context)
     {
         return new OracleServerManager(context.Database.GetDbConnection());
